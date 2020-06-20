92ac8ed47a1afb487f2e8b7d2e18e65b4fc5eb24
joaoppagnan <joaopedroopagnan@gmail.com>
2020-06-19 21:57:05 -0300

    funcao de checar primo implementada

--- src/main.c
+++ src/main.c
@@ -1,17 +1,36 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
+/* Lab 4 - Multiprocess Prime Counter - Joao P. O. Pagnan - 199727 */
 
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+const int tam_buffer=100;
 
-  int x, y;
+int prime_check(unsigned int num){
+  unsigned int primo = 0;
+  if (num == 0){
+    return primo;
+  }
+  else if (num == 1){
+    return primo;
+  }
+  else {
+    for (float divisor = 2; divisor < num; ++divisor){
+      float quociente_flt;
+      int quociente_int;
+      quociente_flt = num/divisor;
+      quociente_int = (int)quociente_flt;
+      if (quociente_int == quociente_flt){
+	return primo;
+      }
+    }
+    primo = 1;
+    return primo;
+  }
+}
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
+int main(){
+  char num_str[tam_buffer];
+  fgets(num_str, tam_buffer, stdin);
+  char* num_ptr = strtok(num_str, " ");
   return 0;
 }
